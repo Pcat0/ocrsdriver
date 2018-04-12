61cc884cfa257a035fd3eb123b24dddf47280b68
Patrick Ambli <patrick.ambli@gmail.com>
2018-04-12 13:19:15 -0500

    Update OCRSDriver.java

--- src/main/java/org/dave/ocrsdriver/OCRSDriver.java
+++ src/main/java/org/dave/ocrsdriver/OCRSDriver.java
@@ -4,7 +4,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = OCRSDriver.MODID, version = OCRSDriver.VERSION, dependencies = "required-after:OpenComputers;required-after:refinedstorage")
+@Mod(modid = OCRSDriver.MODID, version = OCRSDriver.VERSION, acceptableRemoteVersions = "*" , dependencies = "required-after:OpenComputers;required-after:refinedstorage")
 public class OCRSDriver {
     public static final String MODID = "ocrsdriver";
     public static final String VERSION = "1.0";
